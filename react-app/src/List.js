a5ce0158d8836a2f7ca917cf836ee9976b4a23f8
Wesley Coleman <77476122+wesleyjay09@users.noreply.github.com>
2021-04-13 13:55:20 -0500

    Base Components

---
+++ react-app/src/List.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ListItem from './ListItem'
+
+class List extends Component {
+    render() {
+        return (
+            <div>
+<div>This is my list</div>
+<ListItem />
+            </div>
+            
+        )
+    }
+}
+
+export default List
---
+++ react-app/src/ListItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ListItem extends Component {
+    render() {
+        return(
+            <div>This is my ListItem</div>
+        )
+    }
+}
+
+export default ListItem
