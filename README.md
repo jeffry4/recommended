80ab617bc1b338e076a5d572be09e320484b7aca
jeffry4 <jeffry71510@gmail.com>
2021-05-04 17:02:02 -0500

    refact

--- README.md
+++
@@ -1,2 +0,0 @@
-# recommended
-recommended video module
---
+++ db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user:'jeffrydelapena',
+    password:'password',
+    database:'recom',
+    host:'localhost',
+    port:5432
+})
+
+module.exports = pool;
